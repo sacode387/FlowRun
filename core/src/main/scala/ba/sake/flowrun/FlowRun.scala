7d234cf868fb8bbeabd65b5d254c033478f848d7
unknown <sakib@sake.ba>
2021-04-17 01:53:48 +0200

    Extract FlowRun class

--- core/src/main/scala/ba/sake/flowrun/FlowRun.scala
+++ core/src/main/scala/ba/sake/flowrun/FlowRun.scala
@@ -3,89 +3,29 @@ package ba.sake.flowrun
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
 import org.scalajs.dom
-import org.scalajs.dom.window
-import org.scalajs.dom.window.document
-
+import org.getshaka.nativeconverter.NativeConverter
 import scalatags.JsDom.all._
 import reactify._
-
 import ba.sake.flowrun.cytoscape.CytoscapeFlowchart
 import ba.sake.flowrun.eval._
 import ba.sake.flowrun.parse.parseExpr
 
 @JSExportTopLevel("FlowRun")
-class FlowRun() {
-}
+class FlowRun(program: Program, flowRunElements: FlowRunElements) {
 
-object FlowRun {
-  enum Event:
-    case SyntaxError(msg: String)
-    case EvalError(nodeId: String, msg: String)
-    case SyntaxSuccess
-    case EvalOutput(msg: String)
-    case EvalInput(nodeId: String, name: String)
-    case SymbolTableUpdated
-}
+  private val flowrunChannel = Channel[FlowRun.Event]
+  private val programModel = ProgramModel(program)
+  private val cytoscapeFlowchart = CytoscapeFlowchart(programModel, flowrunChannel, flowRunElements.drawArea, flowRunElements.editStatement)
+  private var interpreter = Interpreter(programModel, flowrunChannel)
 
-
-
-
-@JSExportTopLevel("start")
-@main def start(): Unit =
-  window.onload = _ => init()
-
-def init(): Unit = {
-  val flowrunChannel = Channel[FlowRun.Event]
-
-  val container = document.querySelector("#program-wrapper #cy")
-  
-  val editWrapperElem = document.querySelector("#edit-wrapper")
-
-  val runBtn = document.querySelector("#actions-wrapper #run-btn").asInstanceOf[dom.html.Button]
-  val functionsElem = document.querySelector("#actions-wrapper #function-chooser")
-
-  val outputElem = document.querySelector("#run-output")
-
-  val variablesElem = document.querySelector("#variables-output")
-
-  val main = Function("main", None, List(
-    Statement.Begin, 
-    Statement.Declare("1", "x", Expression.Type.Integer, Option("1")),
-    Statement.Call("2", "fun1()"),
-    Statement.Output("3", """ "X in main: "+x """),
- //   Statement.Output("4", "x"),
-    
-  /*  Statement.If("100", "true",
-      Statement.Block("101", List(Statement.Output("101-1", "9"))),
-      Statement.Block("102", List(
-        Statement.If("200", "true",
-          Statement.Block("201", List(Statement.Output("201-1", "9"))),
-          Statement.Block("202", List(Statement.Output("202-1", "9"))),
-        ),
-      )),
-    ),*/
-
-    Statement.End
-  ))
-
-  val fun1 = Function("fun1", None, List(
-    Statement.Begin, 
-    Statement.Declare("1", "x", Expression.Type.Integer, Option("2")),
-    Statement.Output("4", "\"fun1 hello!\""),
-    Statement.Output("5", """ "X in fun1: "+x """),
-    Statement.End
-  ))
-
-  val programModel = ProgramModel(Program("program", main, List(fun1)))
-  val cytoscapeFlowchart = CytoscapeFlowchart(programModel, flowrunChannel, container, editWrapperElem)
-
-  var interpreter = Interpreter(programModel, flowrunChannel)
-
-  var lastRun: String = ""
+  private var lastRun: String = ""
 
   populateFunctions()
 
-  def populateFunctions(): Unit =
+  def json(): js.Any =
+    programModel.ast.toNative
+
+  private def populateFunctions(): Unit =
     val allFunctions = List(programModel.ast.main) ++ programModel.ast.functions
     val selectElem = div(
       allFunctions.map { f =>
@@ -122,14 +62,14 @@ def init(): Unit = {
         populateFunctions()
       })
     )
-    functionsElem.innerText = ""
-    functionsElem.appendChild(selectElem.render)
+    flowRunElements.functionsChooser.innerText = ""
+    flowRunElements.functionsChooser.appendChild(selectElem.render)
 
   // run the program
-  runBtn.onclick = _ => {
+  flowRunElements.runButton.asInstanceOf[dom.html.Button].onclick = _ => {
     cytoscapeFlowchart.clearErrors()
     lastRun = getNowTime
-    outputElem.innerText = s"Started at: $lastRun"
+    flowRunElements.output.innerText = s"Started at: $lastRun"
 
     interpreter = Interpreter(programModel, flowrunChannel) // fresh SymTable etc
     interpreter.run()
@@ -146,18 +86,18 @@ def init(): Unit = {
       output += "\nError: " + msg
       displayError(output)
     case SyntaxSuccess =>
-      outputElem.innerText = ""
-      outputElem.classList.remove("error")
+      flowRunElements.output.innerText = ""
+      flowRunElements.output.classList.remove("error")
     case EvalOutput(output) =>
       val newOutput = pre(output).render
-      outputElem.appendChild(newOutput)
+      flowRunElements.output.appendChild(newOutput)
     case EvalInput(nodeId, name) =>
       evalInput(nodeId, name)
     case SymbolTableUpdated =>
       showVariables()
   }
 
-  def evalInput(nodeId: String, name: String) = {
+  private def evalInput(nodeId: String, name: String) = {
     
     val valueInputElem = input().render
     val valueBtnElem = button("Enter").render
@@ -168,7 +108,7 @@ def init(): Unit = {
         valueBtnElem
       )
     ).render
-    outputElem.appendChild(enterValueDiv)
+    flowRunElements.output.appendChild(enterValueDiv)
 
     valueInputElem.focus()
 
@@ -186,8 +126,8 @@ def init(): Unit = {
         interpreter.continue()
 
         val newOutput = pre(s"Please enter value for '$name': $inputValue").render
-        outputElem.removeChild(enterValueDiv)
-        outputElem.appendChild(newOutput)
+        flowRunElements.output.removeChild(enterValueDiv)
+        flowRunElements.output.appendChild(newOutput)
       } catch {
         case (e: EvalException) => // from symbol table
           displayError(e.getMessage)
@@ -197,18 +137,39 @@ def init(): Unit = {
     }
   }
 
-  def displayError(msg: String): Unit =
-    outputElem.innerText = msg
-    outputElem.classList.add("error")
+  private def displayError(msg: String): Unit =
+    flowRunElements.output.innerText = msg
+    flowRunElements.output.classList.add("error")
 
-  def showVariables(): Unit =
-    variablesElem.innerText = ""
+  private def showVariables(): Unit =
+    flowRunElements.debugVariables.innerText = ""
     val varValues = interpreter.symTab.varSymbols
     varValues.foreach { sym =>
       val symElem = div(s"${sym.key.name}: ${sym.tpe.get} = ${sym.value.getOrElse("")}").render
-      variablesElem.appendChild(symElem)
+      flowRunElements.debugVariables.appendChild(symElem)
     }
 }
+
+object FlowRun:
+  def parseJson(jsonString: String): Program =
+    NativeConverter[Program].fromNative(js.JSON.parse(jsonString))
+
+  enum Event:
+    case SyntaxError(msg: String)
+    case EvalError(nodeId: String, msg: String)
+    case SyntaxSuccess
+    case EvalOutput(msg: String)
+    case EvalInput(nodeId: String, name: String)
+    case SymbolTableUpdated
+
+case class FlowRunElements(
+  drawArea: dom.Element,
+  editStatement: dom.Element,
+  runButton: dom.Element,
+  functionsChooser: dom.Element,
+  output: dom.Element,
+  debugVariables: dom.Element
+)
 
 def getNowTime: String =
   val now = new js.Date()
